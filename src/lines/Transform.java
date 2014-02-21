9637cc8ad6b4cf9cbf8ec64e51411f5e94c6f9e9
Матвей <@asus_smg>
2014-02-21 18:54:30 +0400

    Добавлена документация

--- src/lines/Transform.java
+++ src/lines/Transform.java
@@ -2,13 +2,19 @@ package lines;
 
 import java.awt.geom.Point2D;
 
+/**
+ * Класс, твечающий за проведение трансформаций системы координат: повороты,
+ * параллельные переносы.
+ *
+ * @author Матвей
+ */
 public class Transform {
 
     /**
-     *
-     * @param l
-     * @param angle угол в радианах
-     * @return
+     * Поворот прямой на заданный угол
+     * @param l прямая (immutable).
+     * @param angle угол в радианах.
+     * @return прямая после поворота. 
      */
     public static Line roll(Line l, double angle) {
         double a = l.getA() * StrictMath.cos(angle) + l.getB() * StrictMath.sin(angle);
@@ -17,10 +23,10 @@ public class Transform {
     }
 
     /**
-     *
-     * @param p
-     * @param angle угол в радианах
-     * @return
+     * Поворот точки на заданный угол.
+     * @param p точка (immutable).
+     * @param angle угол в радианах.
+     * @return точка после поворота.
      */
     public static Point2D roll(Point2D p, double angle) {
         double x = p.getX() * StrictMath.cos(angle) + p.getY() * StrictMath.sin(angle);
@@ -29,25 +35,25 @@ public class Transform {
     }
 
     /**
-     *
-     * @param l
-     * @param p
-     * @return
+     * Перенос системы координат для прямой в новый центр <code>centre</code>.
+     * @param l прямая (immutable).
+     * @param centre новый центр координат.
+     * @return прямая после переноса.
      */
-    public static Line move(Line l, Point2D p) {
-        double c = l.getC() + l.getA() * p.getX() + l.getB() * p.getY();
+    public static Line move(Line l, Point2D centre) {
+        double c = l.getC() + l.getA() * centre.getX() + l.getB() * centre.getY();
         return new Line(l.getA(), l.getB(), c);
     }
 
     /**
-     *
-     * @param p
-     * @param cent
+     * Перенос системы координат для точки в новый центр <code>centre</code>.
+     * @param p точка, подлежащая переносу(immutable).
+     * @param centre новый центр координат.
      * @return
      */
-    public static Point2D move(Point2D p, Point2D cent) {
-        double x = p.getX() - cent.getX();
-        double y = p.getY() - cent.getY();
+    public static Point2D move(Point2D p, Point2D centre) {
+        double x = p.getX() - centre.getX();
+        double y = p.getY() - centre.getY();
         return new Point2D.Double(x, y);
     }
 }
