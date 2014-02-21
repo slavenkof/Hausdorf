2b785f5312ca7a4f17bf7de7034fa7426a6ffa5e
Матвей <@asus_smg>
2014-02-21 18:57:59 +0400

    TODO

--- src/lines/log.java
+++ src/lines/log.java
@@ -1,10 +1,11 @@
 package lines;
 
 import java.awt.geom.Point2D;
+//TODO: разобраться с тем, что это за поля какие-то...
 
 public class log {
     public double theta;
     public Point2D sectPoint1;
     public Point2D sectPoint2;
-    public Point2D centerMove;
+    public Point2D centreMove;
 }
