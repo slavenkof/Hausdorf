822630cd06d7e4132fde9fe01537d0b5b68275a9
Матвей <@asus_smg>
2014-02-21 17:05:45 +0400

    Добавлена документация

--- src/triangles/gen/CGenerator.java
+++ src/triangles/gen/CGenerator.java
@@ -2,10 +2,33 @@ package triangles.gen;
 
 import triangles.Polyangle;
 
+/**
+ * Генератор выпуклых многоугольников. Алгоритм состоит из следующих шагов:
+ * <ul>
+ * <li>Случайным образом (на основе "зерна") в заданном прямоугольнике
+ * разбрасываются точки. </li>
+ * <li>При помощи алгоритма Грэхема строится выпуклая этих точек, которая и
+ * является результатом работы алгоритма.</li>
+ * </ul>
+ *
+ * @author Матвей
+ */
 public class CGenerator implements PolGenerator {
 
+    /**
+     * Метод генерации выпуклых многоугольников. На данный момент вызов
+     * делегируется статическому методу класса <code>Polyangle</code>.
+     *
+     * @param qOfP количество точек, разбрасываемое внутри прямоугольника.
+     * @param seed "зерно", используемое для инициализации генератора случайных
+     * чисел.
+     * @param width ширина многоугольника, внутри которого должен раходится
+     * многоугольник.
+     * @param height высота многоугольника.
+     * @return выпуклый многоугольник.
+     */
     @Override
     public Polyangle gen(int qOfP, int seed, int width, int height) {
-        return Polyangle.randGen(qOfP, seed,width, height);
+        return Polyangle.randGen(qOfP, seed, width, height);
     }
 }
