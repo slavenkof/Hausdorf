ea2c49beaf588c94f0b0ac6f82d9092b3346c293
Матвей <@asus_smg>
2014-02-21 16:41:02 +0400

    Добавлена документация

--- src/triangles/test/AlphaC.java
+++ src/triangles/test/AlphaC.java
@@ -3,15 +3,26 @@ package triangles.test;
 import java.io.*;
 import triangles.Polyangle;
 
+/**
+ * Скрипт для подсчета альфа-выпуклости в уже выполненных тесткейсах. В сновную
+ * сборку входить не должен.
+ *
+ * @author Матвей
+ */
 public class AlphaC {
 
+    /**
+     *
+     * @param args
+     * @throws FileNotFoundException
+     */
     public static void main(String[] args) throws FileNotFoundException {
         File cat = new File("D:/Кольцо/Проект/R/Tests/new/TestCase3/Pols/");
         File output = new File("D:/Кольцо/Проект/R/Tests/new/TestCase3/Alpha.txt");
         try (PrintStream out = new PrintStream(new FileOutputStream(output))) {
             String[] list = cat.list();
             for (String s : list) {
-                Polyangle a = Polyangle.read(new File(cat+"/"+s), false);
+                Polyangle a = Polyangle.read(new File(cat + "/" + s), false);
                 out.println("Alpha: " + a.getAlpha());
             }
         }
